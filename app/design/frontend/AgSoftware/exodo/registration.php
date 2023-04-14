bd6246ac4b6e44f036e3d7271caab18c7d751125
angel coyasos <angelcotr@gmail.com>
2023-04-14 15:54:01 -0500

    migrate-to-theme: add migration to theme AgSoftware_exodo

--- app/design/frontend/AgSoftware/exodo/registration.php
+++ app/design/frontend/AgSoftware/exodo/registration.php
@@ -6,4 +6,4 @@
 
 use \Magento\Framework\Component\ComponentRegistrar;
 
-ComponentRegistrar::register(ComponentRegistrar::THEME, 'frontend/Weltpixel/clone', __DIR__);
+ComponentRegistrar::register(ComponentRegistrar::THEME, 'frontend/AgSoftware/exodo', __DIR__);
